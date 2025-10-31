e32bd31cadadcbd56b8d61cbd0cf91a437fe02b1
Mark Hallap <hallapmark@gmail.com>
2025-10-31 07:52:32 +0200

    Add CourseKeys enum
    Reduces stringly typing

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,15 +1,42 @@
 import { Link } from "react-router-dom"
 import "./Courses.css"
+import { useState } from "react"
+
+// enum approximation
+const CourseKeys = Object.freeze({
+	Udemy: "udemy",
+	Coursera: "coursera",
+	Codecademy: "codecademy",
+	Udacity: "udacity"
+})
 
 function Courses() {
+
+  const [kursus, uuendaKursus] = useState("Valimata");
+  
   return (
     <>
-      <div className="back-container">
+      <div className="courses-back-container">
         <Link to="/">
           <button>Tagasi</button>
         </Link>
       </div>
-      <div>Kursuste sisu</div>
+        <button 
+        className={`kursus ${kursus === CourseKeys.Udemy ? 'aktiivne' : undefined}`}
+        onClick={() => uuendaKursus(CourseKeys.Udemy)}>Udemy</button>
+        <button
+        className={`kursus ${kursus === CourseKeys.Coursera ? 'aktiivne' : undefined}`}
+        onClick={() => uuendaKursus(CourseKeys.Coursera)}>Coursera</button>
+        <button
+        className={`kursus ${kursus === CourseKeys.Codecademy ? 'aktiivne' : undefined}`}
+        onClick={() => uuendaKursus(CourseKeys.Codecademy)}>Codecademy</button>
+        <button
+        className={`kursus ${kursus === CourseKeys.Udacity ? 'aktiivne' : undefined}`}
+        onClick={() => uuendaKursus(CourseKeys.Udacity)}>Udacity</button>
+        {kursus === CourseKeys.Udemy && <div>Siin on kirjeldus (loend) Udemy.com läbitud kursustest</div>}
+        {kursus === CourseKeys.Coursera && <div>Siin on kirjeldus (loend) Coursera.org läbitud kursustest</div>}
+        {kursus === CourseKeys.Codecademy && <div>Siin on kirjeldus (loend) Udemy.com läbitud kursustest</div>}
+        {kursus === CourseKeys.Udacity && <div>Siin on kirjeldus (loend) Udacity.com läbitud kursustest</div>}
     </>
   )
 }
